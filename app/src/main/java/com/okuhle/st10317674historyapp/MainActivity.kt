c30cfe56d15dd06d61334e263d1fa540c24a337b
lab_services_student <ST10317674@vcconnect.edu.za>
2024-04-05 21:43:15 +0200

    History app final

--- app/src/main/java/com/okuhle/st10317674historyapp/MainActivity.kt
+++ app/src/main/java/com/okuhle/st10317674historyapp/MainActivity.kt
@@ -1,45 +1,79 @@
 package com.okuhle.st10317674historyapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var buttonGenerate: Button
-    private lateinit var buttonClear: Button
-    private lateinit var editTextAge: EditText
-    private lateinit var editTextInfo: EditText
+    //declare
+    private lateinit var searchButton: Button
+    private lateinit var resultTextView:TextView
+    private lateinit var ageInput: EditText
+    private lateinit var clearButton:Button
 
 
+
+
+
+
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editTextAge = findViewById(R.id.editTextAge)
-        buttonClear = findViewById(R.id.button2)
-        buttonGenerate = findViewById(R.id.button)
-        editTextInfo = findViewById(R.id.editTextInfo)
 
-        buttonGenerate.setOnClickListener {
-            val name = editTextAge.text.toString().trim()
-            val ageStr = editTextInfo.text.toString().trim()
 
-            if (name.isEmpty() || ageStr.isEmpty()) {
-                Toast.makeText(this, "Please enter the age number", Toast.LENGTH_LONG).show()
-                Log.e("Message Log", "User did not enter the correct number")
-            }else {
-                val ageStr = ageStr.toIntOrNull()
-                if (ageStr == null || ageStr  < 0) {
-                    Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show()
-                }else{
-                    Log.d("Enter Age: $age")
-                    Log.d("MainActivity : $ageStr")
-                    Toast.makeText(this,"The correct Age has been successfully logged", Toast.LENGTH_LONG).show()
+
+        searchButton= findViewById(R.id.searchButton)
+        clearButton = findViewById(R.id.clearButton)
+        ageInput = findViewById(R.id.ageInput)
+        resultTextView= findViewById(R.id.resultTextView)
+
+        searchButton.setOnClickListener{
+//create a variable of age to get value from user
+            val age = ageInput.text.toString().toIntOrNull()
+
+//if age is not null and age is in the range between
+            if (age != null && age in 20 .. 100) {
+//when age is 30 display Thato else when is .. and so on
+                val famousfiguresName = when (age) {
+                    36 -> "Marilyn Monroe" +
+                            "She was known for playing comic \"blonde bombshell\" characters and became one of the most popular sex symbols of the 1950s and early 1960s"
+                    56 -> "Abraham Lincoln" +
+                            "A US President also wrote poetry and has been called the country’s only poet president."
+                    39 -> "Martin Luther King" +
+                            "An American civil rights campaigner also He was instrumental in the passage of the Civil Rights Act of 1964 and the Voting Rights of 1965 "
+                    41 -> "Kobe Bryant" +
+                            "Kobe Bryant was named after one of the world's most famous and expensive type of beef"
+                    67 -> "Joe Frazier" +
+                            "He was the undisputed heavyweight champion for three consecutive years from 1970-1973"
+                    76 -> "Albert Einstein " +
+                            "He received his federal teaching diploma in 1990 and later developed the theory of relativity. "
+                    44 -> "Pablo Escobar" +
+                            "He was considered the wealthiest criminal in history."
+                    68 -> "Akira Toriyama " +
+                            "He is a Japanese manga artist and character designer"
+                    91 -> "Pablo Picasso" +
+                            "He was a child prodigy who finished his first painting at 9 years old and his father gave up art after seeing his son’s talents. "
+                    74 -> "Muhamad Ali" +
+                            "American Boxer and civil rights campaigner He was also well known for his clever rhymes and was named \"Sportsman of the Century\" by Sports Illustrated magazine in 1999."
+                    else -> null
                 }
+                val message = if (famousfiguresName != null) " The celebrity's name is $famousfiguresName."
+                else "No famous figure found with the entered age."
+                resultTextView.text = message
+            } else{
+                resultTextView.text = "Invalid input. Please enter a valid age between 20 and 100."
+
             }
+        }
+
+        clearButton.setOnClickListener{
+            ageInput.text.clear()
+            resultTextView.text = ""
         }
     }
 }
